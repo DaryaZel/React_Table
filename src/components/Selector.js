57b7f9c1908360cb7f89d980dc4fae5bcde67248
Darya <darya@MacBook-Pro-Aliaksei-2.local>
2021-09-16 21:17:54 +0300

    add select options sorting

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,10 +1,20 @@
 export function Selector({ users, changeSelectedState }) {
-    const statesArray = ['ALL']
+    const statesArray = []
     for (let i = 0; i < users.length; i++) {
         if (!statesArray.includes(users[i].adress.state)) {
             statesArray.push(users[i].adress.state)
         }
     }
+    statesArray.sort((a, b) => {
+        if (a < b) {
+            return -1;
+        }
+        if (a > b) {
+            return 1;
+        }
+        return 0;
+    });
+    statesArray.unshift('ALL')
 
     return (
         <select className='filters-button' name="select" onChange={(e) => changeSelectedState(e.target.value)} >
